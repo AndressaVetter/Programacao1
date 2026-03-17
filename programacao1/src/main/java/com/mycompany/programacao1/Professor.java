d3aa4857396a5577b973fb44056bc7da026d1bbc
aluno <aluno@lab01-07>
2026-03-17 14:11:12 -0300

    17/03/26

---
+++ programacao1/src/main/java/com/mycompany/programacao1/Professor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programacao1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Professor {
+    
+    private static String nome;
+    private int SIAPE;
+    private String email;
+    private int telefone;
+    private String titulacao;
+    private String area;
+    private Date dataIngresso;
+    private String formacao;
+    
+    
+}
