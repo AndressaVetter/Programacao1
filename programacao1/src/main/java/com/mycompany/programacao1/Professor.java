557b5e94ab98f8b298399e99c33e860ae7d75304
aluno <aluno@lab01-07>
2026-03-23 09:22:14 -0300

    23/03/26

--- programacao1/src/main/java/com/mycompany/programacao1/Professor.java
+++ programacao1/src/main/java/com/mycompany/programacao1/Professor.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class Professor {
     
     private static String nome;
-    private int SIAPE;
+    private int siape;
     private String email;
     private int telefone;
     private String titulacao;
@@ -21,5 +21,50 @@ public class Professor {
     private Date dataIngresso;
     private String formacao;
     
-    
+    public Professor(){
+        nome = "Nobody";
+        
+    }
+    public int getSIAPE(){
+        return siape;
+    }
+    public void setSIAPE(int _siape){
+    siape = _siape;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String _email){
+        email = _email;
+    }
+    public int getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(int _telefone){
+        telefone = _telefone;
+    }
+    public String getTitulacao(){
+        return titulacao;
+    }
+    public void setTitulacao(String _titulacao){
+        titulacao = _titulacao;
+    }
+    public String getArea(){
+        return area;
+    }
+    public void setArea(String _area){
+        area = _area;
+    }
+    public Date getDataIngresso (){
+        return dataIngresso;
+    }
+    public void setDataIngresso (Date _dataIngresso){
+        dataIngresso = _dataIngresso;
+    }
+    public String getFormacao(){
+        return formacao;
+    }
+    public void setFormacao(String _formacao){
+        formacao = _formacao;
+    }
 }
