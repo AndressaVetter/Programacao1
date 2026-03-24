390c969d5d4bb0fc8f246f18a94c87aee959fac2
aluno <aluno@lab01-07>
2026-03-24 14:19:04 -0300

    24/03/26

---
+++ programacao1/src/main/java/com/mycompany/programacao1/Turma.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programacao1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Turma {
+
+    private String nomeTurma;
+    private int anoIngresso;
+    private String sala;
+    
+    
+    
+//* disciplina = composição
+//* professor = agregação
+//* aluno = composição - 1..*
+    //* Turma = 1
+
+    
+}
